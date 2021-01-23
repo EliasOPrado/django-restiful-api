e35edd1fffb0b9f4824885567b8ee22b9b1448b2
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-23 02:24:05 -0300

    added filters

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracao
+from rest_framework import filters
 from .serializers import AtracaoSerializer
 
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id', 'nome', 'descricao', 'foto']
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -9,4 +9,18 @@ class PontoTuristicoViewSet(viewsets.ModelViewSet):
     #queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset .objects.filter(nome__iexact=nome)
+
+        if descricao:
+            queryset = queryset.objects.filter(descricao=descricao)
+
+        return queryset
