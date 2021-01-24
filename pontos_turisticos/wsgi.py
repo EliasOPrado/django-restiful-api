a87faf1f91bb8bf153c829a16bf662ad5e9fbdc2
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-24 00:46:49 -0300

    making it better

--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
+application = get_wsgi_application()
