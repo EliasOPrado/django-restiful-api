09ba7fa56254b5d0f50d47c6b60388a242e433c8
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-24 22:44:40 -0300

    added changes to settins

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://pontos-turisticos-drf-api.herokuapp.com/', '127.0.0.1']
 
